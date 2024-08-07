5eeeda32996f4dc765706915f0a4326ae5848a5f
GitHub Action <action@github.com>
2024-08-07 05:25:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/gs618/myfirstintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/gs618/myfirstintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gs618.myfirstintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gs618/myfirstintellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gs618/myfirstintellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gs618.myfirstintellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
